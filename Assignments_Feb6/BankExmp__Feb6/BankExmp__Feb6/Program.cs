6e1c38832fd052b2db025279f22c3c2b881d80ec
K Sumanth Reddy <K.Reddy2@in.ey.com>
2023-02-11 14:25:03 +0530

    files added

---
+++ Assignments_Feb6/BankExmp__Feb6/BankExmp__Feb6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankExmp__Feb6
+{
+    public class Bank
+    {
+        protected void PersonalDetails()
+        {
+            Console.WriteLine("PersonalDetails of the Customer");
+        }
+        private void SalaryDetails()
+        {
+            Console.WriteLine("SalaryDetails");
+        }
+    }
+    class ThirdParty:Bank           //This Card details is inherited from the parent class Bank
+    {
+        static void Main(string[] args)
+        {
+            ThirdParty card = new ThirdParty();
+            card.PersonalDetails();
+        }
+    }
+}
+
---
+++ Assignments_Feb6/Day3__Feb6/Day3__Feb6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3__Feb6
+{
+    
+        class Bank
+        {
+            public long AccountNumber;
+            public string Name;
+            public int Balance;
+
+            public void GetBalance()
+            {
+            }
+            public void WithdrawAmount()
+            {
+            }
+            public void Deposit()
+            {
+            }
+        }
+    }
+
